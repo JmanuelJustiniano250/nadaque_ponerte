0a9351b80b8bcd4d8b82625b5e898425b7f77c60
JmanuelJustiniano250 <m.justiniano@prometheus.technology>
2018-03-05 02:21:36 -0400

    Create vigentes2.php

---
+++ views/cuenta/vigentes2.php
@@ -0,0 +1,78 @@
+<?php
+
+use himiklab\thumbnail\EasyThumbnailImage;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Anuncios */
+
+
+?>
+
+<div class="col-md-4 col-sm-6">
+
+    <div class="cajadeanuncio">
+        <div class="imgprove project-gal">
+            <div class="post-gal">
+                <?=
+                EasyThumbnailImage::thumbnailImg(
+                    Yii::getAlias('@webroot/imagen/anuncios/') . $model['foto'],
+                    283,
+                    300,
+                    EasyThumbnailImage::THUMBNAIL_OUTBOUND,
+                    ['style' => 'margin: 0 auto;', 'class' => 'img-responsive']
+                );
+                ?>
+
+
+            </div>
+        </div>
+
+        <div class="cajadesc">
+
+            <div class="row">
+                <div class="list1 cajapro">
+
+                    <h3 class="text-center"> <?= $model['titulo'] ?></h3>
+
+
+                    <div class="col-sm-6 col-xs-12 dl2" style="padding-right: 0">
+                        <p class="nobor">Cod: <?= $model['codigo'] ?> </p>
+
+                    </div>
+
+                   <!-- <div class="col-sm-6 col-xs-12 dl2" style="padding-right: 0">
+                        <p class="nobor"><span>Cod. Paquete: </span>
+                            <?/*= $item['codigo'] */?> </p>
+
+                    </div>-->
+
+                </div>
+
+                <div class="list2">
+
+
+                    <p class="text-center nobor">
+
+                        <br>
+
+                        Publicado desde <span> <?= \app\components\Funcions::fecha($model->paquete['fecha_pago'],true,true) ?></span>
+                        <br>
+                        <?php
+                        $fecha_expiracion = mktime(0, 0, 0, date("m", $model->paquete['fecha_pago']), date("d", $model->paquete['fecha_pago'])+(isset($model->paquete->paquete['tiempo_vida'])?$model->paquete->paquete['tiempo_vida']:0), date("Y", $model->paquete['fecha_pago']));
+                        $days = (strtotime($fecha_expiracion) - time())/(3600*24);
+                        ?>
+
+                        Dias remanentes <span> <?= $days?></span>
+
+
+                    </p>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
