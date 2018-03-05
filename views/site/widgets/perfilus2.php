40b592a3594e884a91855e83914593e4e3cbad74
webmasterj@mercado.com <8193367>
2018-03-05 11:47:44 -0400

    Cambios

---
+++ views/site/widgets/perfilus2.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Url;
+?>
+
+<div class="bread">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+
+                <p class="poscio">
+                    <a href="<?php echo Url::to('../site/opciones') ?>" class="anunciocrear">Crea tu anuncio</a>
+
+                    <i class="fa fa-home" aria-hidden="true"></i></p>
+            </div>
+        </div>
+    </div>
+
+</div>
